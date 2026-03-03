0aa0f553b4c968d1eb59ca194fba768764e26380
josedona88 <josedonairemata8@gmail.com>
2026-03-03 12:34:41 +0100

    Add files via upload

--- proyectos/MainAlmacen.java
+++ proyectos/MainAlmacen.java
@@ -1,5 +1,9 @@
 package proyectos;
 
+import ej2.Producto;
+
 public class MainAlmacen {
 
-}
+}  public static void main(String[] args) {
+    Producto p1 = new Producto("ALM-1234", "Ratón", 10);
+    Producto p2 = new Producto("XXX-9999", "Teclado", 20);
