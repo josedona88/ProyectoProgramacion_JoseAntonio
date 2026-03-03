853e8e18ed7d354b255660fad9a405ea5afb9023
josedona88 <josedonairemata8@gmail.com>
2026-03-03 12:38:40 +0100

    COMMIT3

--- proyectos/MainAlmacen.java
+++ proyectos/MainAlmacen.java
@@ -1,9 +1,13 @@
 package proyectos;
 
-import ej2.Producto;
+
 
 public class MainAlmacen {
 
-}  public static void main(String[] args) {
+
+public static void main(String[] args) {
     Producto p1 = new Producto("ALM-1234", "Ratón", 10);
     Producto p2 = new Producto("XXX-9999", "Teclado", 20);
+  
+      System.out.println(p1);
+   
